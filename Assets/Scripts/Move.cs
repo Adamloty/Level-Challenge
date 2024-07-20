e86db0094b1e54c8c327e1c93152a164c58527a5
Adamlotfy <161876835+Adamloty@users.noreply.github.com>
2024-07-20 16:44:18 +0300

    تحسين في حركة الاعب

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    // Start is called before the first frame update
     private float horz;
-    [SerializeField] private float speed=50;
-    [SerializeField] private float Jump=10;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool isJumping;
-    [SerializeField] private bool isGroundedFunc;
-    [SerializeField] private bool JumpwithRigidbody;
     private SpriteRenderer sprite;
     private Animator anim;
-   // private float vert;
+
     void Start()
     {
-        rb= GetComponent<Rigidbody2D>();
-        sprite= GetComponent<SpriteRenderer>();
-        anim= GetComponent<Animator>();
-       // Jump = 0;
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         horz = Input.GetAxis("Horizontal");
-        if(horz<0)
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        if (horz < 0)
         {
-            sprite.flipX= true;
+            sprite.flipX = true;
             anim.SetBool("Run", true);
         }
-        else if(horz>0)
+        else if (horz > 0)
         {
             sprite.flipX = false;
             anim.SetBool("Run", true);
@@ -42,55 +43,29 @@ public class Move : MonoBehaviour
         {
             anim.SetBool("Run", false);
         }
-        //vert = Input.GetAxis("Vertical");
-        if(isGroundedFunc)
-        {
-            if (isGrounded)
-            {
-                transform.position += new Vector3(horz * speed * Time.deltaTime, 0, 0);
-                //rb.velocity = new Vector3(horz * speed * Time.deltaTime, Jump);
-            }
-        }
-        else
-        {
-            transform.position += new Vector3(horz * speed * Time.deltaTime, 0, 0);
-            // rb.velocity = new Vector3(horz * speed * Time.deltaTime, Jump);
-        }
-   
-        if(Input.GetKeyDown(KeyCode.Space)&&!isJumping)
-        {
-            if(JumpwithRigidbody)
-            {
-                rb.velocity = new Vector3(0, Jump);
-            }
-            else
-            {
-                transform.position += new Vector3(0,Jump, 0);
-            }
-          
-          //  anim.SetBool("Down", true);
-            //قفز اللاعب
-            isJumping = true;
-            //لم يلمس الأرض بعد
-            isGrounded = false;
-        } 
+
+        Vector3 movement = new Vector3(horz * speed * Time.deltaTime, 0, 0);
+        transform.position += movement;
     }
-    
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isJumping = true;
+            isGrounded = false;
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Ground tagمع مراعاة وضع علي الأرضية 
         if (collision.collider.CompareTag("Ground"))
         {
-           // anim.SetBool("Down", false);
-            //يلمس الأرض
             isGrounded = true;
-            //لم يقفز اللاعب بعد
-            isJumping=false;
-        }
-        else
-        {
-            //لا يلمس الأرض
-            isGrounded= false;
+            isJumping = false;
         }
     }
 }
+
+
